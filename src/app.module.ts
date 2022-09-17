64b542342bc53fc34041c3ab77f74649bbe454ac
KurtDev599 <sw536880@naver.com>
2022-09-17 14:05:58 +0900

    nestjs/config init setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { BoardsModule } from './boards/boards.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeormConfig } from './config/typeorm.config';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeormConfig), BoardsModule],
+  imports: [
+    TypeOrmModule.forRoot(typeormConfig),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+    }),
+    BoardsModule,
+  ],
   controllers: [],
   providers: [],
 })
