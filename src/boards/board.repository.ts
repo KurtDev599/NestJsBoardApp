81f50ac512a969d59985795a7b4648170608aead
KurtDev599 <sw536880@naver.com>
2022-09-21 23:54:55 +0900

    typeORM Customer EntityRepository Decorator

--- src/boards/board.repository.ts
+++ src/boards/board.repository.ts
@@ -1,5 +1,6 @@
-import { EntityRepository, Repository } from 'typeorm';
 import { Board } from './board.entitiy';
+import { CustomRepository } from './custom/typeorm-ex.decorator';
+import { Repository } from 'typeorm';
 
-@EntityRepository(Board)
+@CustomRepository(Board)
 export class BoardRepository extends Repository<Board> {}
