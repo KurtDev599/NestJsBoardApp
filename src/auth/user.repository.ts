fb0917683fb2b0e0b35fdcaff90142871e899420
KurtDev599 <sw536880@naver.com>
2022-10-02 17:43:29 +0900

    Implement encryption using bcryptJS

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -5,15 +5,19 @@ import {
   ConflictException,
   InternalServerErrorException,
 } from '@nestjs/common';
+import * as bcrypt from 'bcryptjs';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
   async createUser(authCredentialDto: AuthCredentialDto): Promise<void> {
     const { userName, password } = authCredentialDto;
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
     const user = this.create({
       userName,
-      password,
+      password: hashedPassword,
     });
+
     try {
       await this.save(user);
     } catch (error) {
